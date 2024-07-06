e912afa66edf5de062690a0625d73ca3e1b9cadf
minyugyeong <alsdbrud123@naver.com>
2024-07-06 17:14:23 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/logourtrip/LogourtripApplication.java
@@ -0,0 +1,13 @@
+package com.project.logourtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogourtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogourtripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/logourtrip/LogourtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.logourtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogourtripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
